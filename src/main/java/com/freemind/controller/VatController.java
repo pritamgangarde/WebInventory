2eff61deacffecf80dbd690ca167f86b381cb969
Sagar Jadhav <sagarj28@gmail.com>
2016-02-14 12:29:11 +0530

    Revert "Commit"
    This reverts commit 87c1be2f30bd3c33cb03b7145cdb4fc2fae6a925.

--- src/main/java/com/freemind/controller/VatController.java
+++ src/main/java/com/freemind/controller/VatController.java
@@ -1,70 +1,17 @@
 package com.freemind.controller;
 
-import java.util.List;
-import java.util.Locale;
-
 import javax.servlet.http.HttpServletRequest;
-import javax.validation.Valid;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.freemind.model.CategoryModel;
-import com.freemind.model.Godown;
-import com.freemind.model.Vat;
-import com.freemind.services.VatService;
-
-@Controller("/addVat")
+@Controller
 public class VatController {
-	@Autowired
-	VatService vatservice;
-	@Autowired
-	private MessageSource messageSource;
-	@RequestMapping(method = RequestMethod.GET)
-	public ModelAndView showVat(Model uiModel, ModelMap model,
-			HttpServletRequest httpServletRequest) {
-		System.out.println(uiModel);
-		List<Vat> vatList = vatservice.getAllVatList();
-		uiModel.addAttribute("vatlist", vatList);
+	@RequestMapping(value = "/addVat", method = RequestMethod.GET)
+	public ModelAndView godown(Model model, HttpServletRequest request) {
 		return new ModelAndView("Inventory/Vat");
 	}
-	@RequestMapping(method = RequestMethod.POST)
-	public ModelAndView addVat(
-			@ModelAttribute("addVat") @Valid Vat vatModel,	
-			BindingResult bindingResult, Model uiModel,
-			HttpServletRequest httpServletRequest,
-			RedirectAttributes redirectAttributes, Locale locale) {
-
-		vatservice.save(vatModel);
-		List<Vat> addCategorylist = vatservice.getAllVatList();
-		uiModel.addAttribute("vatlist", addCategorylist);
-		uiModel.addAttribute("msgType", "1");
-		uiModel.addAttribute("msg", "Vat Added Successfully....!!!!");
-		return new ModelAndView("Inventory/Vat");
-	}
-	
-	@RequestMapping(value = "{id}", params = "deletevat", method = RequestMethod.GET)
-	public ModelAndView deleteVat(@PathVariable("id") Integer id,
-			Model uiModel, HttpServletRequest httpServletRequest) {
-		
-		Vat godownmodel =  vatservice.getVatById(id);
-		godownmodel.setActive(false);
-		vatservice.update(godownmodel);
-		uiModel.addAttribute("vatlist", vatservice.getAllVatList());
-		uiModel.addAttribute("msgType", "3");
-		uiModel.addAttribute("msg", "Vat Deleted Successfully....!!!!");
-		return new ModelAndView("Inventory/Vat");
-
-	}
-
 }
